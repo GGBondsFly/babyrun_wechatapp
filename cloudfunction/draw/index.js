b571590cbf7ad39d32f1768b585127ea58a3f588
jinqiang <ks_strong@163.com>
2024-01-11 17:53:04 +0800

    请求重试开发完成

--- cloudfunction/draw/index.js
+++ cloudfunction/draw/index.js
@@ -5,6 +5,7 @@ const fs = require('fs')
 
 cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
 const db = cloud.database()
+const max_retry_times = 3
 
 // 云函数入口函数
 exports.main = async (event, context) => {
@@ -25,17 +26,35 @@ async function drawfunc (id, fileid, prompt){
     console.info("get file url success")
     return callapi(id, res.fileList[0].tempFileURL, prompt)
   }).catch(function(err) {
-    // 获取文件url失败
+    // 获取文件url失败`
     console.info("get file url failed", err)
     return updateDb(id, {status: -1})
   })
 }
 
-async function mockDrawApi(options){
+async function mockDrawApi(options, times=3){
   return JSON.stringify({
     image_url:"https://midjourney.cdn.zhishuyun.com/attachments/1124768570157564029/1190124531227054160/cvye346_1baby_chinese_baby_1_years_old_id1676928_a5db8767-0c91-4b3c-8259-950572e79e9b.png?ex=65a0a8d7&is=658e33d7&hm=99388dcd7cc8fd06b3d155e928d895222f66d7b18e28f80cd1ad341d48dac8ca&width=1024&height=1024",
     image_width:1024,
     image_height:1024
+  })
+}
+
+async function request_server(options, times=3){
+  let total = times
+  return new Promise(function(resolve, reject) {
+    function attempt () {
+      request(options).then(resolve).catch(function(erro) {
+        times--
+        if (0 == times) {
+          reject(erro)
+        } else {
+          console.log("第", total-times,  "次生成失败, 重新尝试：", erro)
+          attempt()
+        }
+      })
+    }
+      attempt()
   })
 }
 
@@ -53,23 +72,8 @@ async function callapi (id, fileurl, prompt){
     })
   };
   console.info("post to draw with ", options)
-  //set Rotation training mode five times.if success, return 
-  // let count = 0
-  // let ret = ""
-  // while(count < 5){
-  //   ret = await request(options)
-  //   console.info("draw image: ", ret)
-  //   let retobj = JSON.parse(ret)
-  //   if(retobj.success == true){
-  //     console.info("success draw image: ", ret)
-  //     return savephoto(id, ret)
-  //   }
-  //   count++
-  // }
-  // console.info("failed post to draw image: ", ret)
-  // return updateDb(id, {status: -2})
   return mockDrawApi(options).then(function (res) {
-//   return request(options).then(function (res) {
+  // return request_server(options).then(function (res) {
     // 请求成功，上传文件
     let ret = JSON.parse(res)
     console.info("success draw image: ", ret)
